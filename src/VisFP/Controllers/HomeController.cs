dea3f6dcbe57863dbf155bac2192fbead60a212a
ashtanov <aleksandr-shtanov@yandex.ru>
2017-05-02 14:51:34 +0300

    Добавление одиночного ученика в группу

--- src/VisFP/Controllers/HomeController.cs
+++ src/VisFP/Controllers/HomeController.cs
@@ -26,28 +26,28 @@ namespace VisFP.Controllers
         public async Task<IActionResult> Index()
         {
             // (t 1 , p 2 ), (t 1 , p 3 ), (t 1 , p 5 ), (t 2 , p 5 ), (t 3 , p 4 ), (t 4 , p 2 ), (t 4 , p 3 ) 
-            var petryNet = new PetryNet(
-                P: new[] { "p1", "p2", "p3", "p4", "p5" },
-                T: new[] { "t1", "t2", "t3", "t4" },
-                F: new[]
-                {
-                    new PetryFlowLink { from = "p1", to = "t1" },
-                    new PetryFlowLink { from = "p2", to = "t2" },
-                    new PetryFlowLink { from = "p3", to = "t2" },
-                    new PetryFlowLink { from = "p3", to = "t3", w="2" },
-                    new PetryFlowLink { from = "p4", to = "t4" },
-                    new PetryFlowLink { from = "p5", to = "t2" },
-                    new PetryFlowLink { from = "t1", to = "p2" },
-                    new PetryFlowLink { from = "t1", to = "p3" },
-                    new PetryFlowLink { from = "t1", to = "p5" },
-                    new PetryFlowLink { from = "t2", to = "p5" },
-                    new PetryFlowLink { from = "t3", to = "p4" },
-                    new PetryFlowLink { from = "t4", to = "p2" },
-                    new PetryFlowLink { from = "t4", to = "p3" }
-                },
-                markup: new[] { "0", "100", "0", "0", "1" });
-            var o = petryNet.Serialize();
-            PetryNetGraph png = new PetryNetGraph(PetryNet.Deserialize(o));
+            //var petryNet = new PetryNet(
+            //    P: new[] { "p1", "p2", "p3", "p4", "p5" },
+            //    T: new[] { "t1", "t2", "t3", "t4" },
+            //    F: new[]
+            //    {
+            //        new PetryFlowLink { from = "p1", to = "t1" },
+            //        new PetryFlowLink { from = "p2", to = "t2" },
+            //        new PetryFlowLink { from = "p3", to = "t2" },
+            //        new PetryFlowLink { from = "p3", to = "t3", w="2" },
+            //        new PetryFlowLink { from = "p4", to = "t4" },
+            //        new PetryFlowLink { from = "p5", to = "t2" },
+            //        new PetryFlowLink { from = "t1", to = "p2" },
+            //        new PetryFlowLink { from = "t1", to = "p3" },
+            //        new PetryFlowLink { from = "t1", to = "p5" },
+            //        new PetryFlowLink { from = "t2", to = "p5" },
+            //        new PetryFlowLink { from = "t3", to = "p4" },
+            //        new PetryFlowLink { from = "t4", to = "p2" },
+            //        new PetryFlowLink { from = "t4", to = "p3" }
+            //    },
+            //    markup: new[] { "0", "100", "0", "0", "1" });
+            //var o = petryNet.Serialize();
+            //PetryNetGraph png = new PetryNetGraph(PetryNet.Deserialize(o));
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
@@ -75,7 +75,6 @@ namespace VisFP.Controllers
                     {
                         IsAdmin = await _userManager.IsInRoleAsync(user, Enum.GetName(typeof(DbRole), DbRole.Admin)),
                         IsTeacher = await _userManager.IsInRoleAsync(user, Enum.GetName(typeof(DbRole), DbRole.Teacher)),
-                        png = png,
                         TaskTypes = ttmodels
                     });
             }
---
+++ src/VisFP/Models/AccountViewModels/CreateStudentViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using VisFP.Data.DBModels;
+
+namespace VisFP.Models.AccountViewModels
+{
+    public class CreateStudentViewModel
+    {
+        [Required]
+        [Display(Name = "ФИО")]
+        public string RealName { get; set; }
+        [Display(Name = "Дополнительная информация")]
+        public string Meta { get; set; }
+        public string GroupName { get; set; }
+        public Guid GroupId { get; set; }
+    }
+}
